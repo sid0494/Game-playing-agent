eda6ef049afb77c355c5dc237986e9b78712b66b
sid0494 <siddharth201201027@gmail.com>
2017-03-01 11:31:33 +0530

    Final Commit for Python code

--- homework.py
+++ homework.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import time
 
 class cell:
@@ -13,13 +13,14 @@ def aplha_beta_decision(grid, values, players, N, depth, cutoff):
 	v = -9999
 	aplha = -9999
 	beta = 9999
-	for s in possible_stakes(grid, players[0], N):
+	stakes, raids = possible_stakes_and_raids(grid, players[0], N)
+	for s in stakes:
 		new_value = min_value_ab(execute_stake(s[1], s[2], grid, players[0], N), values, players, N, depth + 1, cutoff, aplha, beta)
 		if v < new_value:
 			v = new_value
 			move = [s[0] + " Stake", (s[1], s[2])]
 			# print move
-	for r in possible_raids(grid, players[0], N):
+	for r in raids:
 		new_value = min_value_ab(execute_raid(r[1], r[2], grid, players[0], N), values, players, N, depth + 1, cutoff, aplha, beta)
 		if v < new_value:
 			v = new_value
@@ -34,7 +35,10 @@ def min_value_ab(grid, values, players, N, depth, cutoff, aplha, beta):
 		return grid_value(grid, values, players[0], N)
 	else:
 		v = 9999
-		for s in possible_stakes(grid, players[1], N):
+		stakes, raids = possible_stakes_and_raids(grid, players[1], N)
+		if stakes == [] and raids == []:
+			return grid_value(grid, values, players[0], N)
+		for s in stakes:
 			new_value = max_value_ab(execute_stake(s[1], s[2], grid, players[1], N), values, players, N, depth + 1, cutoff, aplha, beta)
 			if v > new_value:
 				v = new_value
@@ -42,7 +46,7 @@ def min_value_ab(grid, values, players, N, depth, cutoff, aplha, beta):
 					return v
 				if beta > v:
 					beta = v
-		for r in possible_raids(grid, players[1], N):
+		for r in raids:
 			new_value = max_value_ab(execute_raid(r[1], r[2], grid, players[1], N), values, players, N, depth + 1, cutoff, aplha, beta)
 			if v > new_value:
 				v = new_value
@@ -58,7 +62,10 @@ def max_value_ab(grid, values, players, N, depth, cutoff, aplha, beta):
 		return grid_value(grid, values, players[0], N)
 	else:
 		v = -9999
-		for s in possible_stakes(grid, players[0], N):
+		stakes, raids = possible_stakes_and_raids(grid, players[0], N)
+		if stakes == [] and raids == []:
+			return grid_value(grid, values, players[0], N)
+		for s in stakes:
 			new_value = min_value_ab(execute_stake(s[1], s[2], grid, players[0], N), values, players, N, depth + 1, cutoff, aplha, beta)
 			if v < new_value:
 				v = new_value
@@ -66,7 +73,7 @@ def max_value_ab(grid, values, players, N, depth, cutoff, aplha, beta):
 					return v
 				if aplha < v:
 					alpha = v
-		for r in possible_raids(grid, players[0], N):
+		for r in raids:
 			new_value = min_value_ab(execute_raid(r[1], r[2], grid, players[0], N), values, players, N, depth + 1, cutoff, aplha, beta)
 			if v < new_value:
 				v = new_value
@@ -80,13 +87,14 @@ def minimax_decision(grid, values, players, N, depth, cutoff):
 
 	move = None
 	v = -9999
-	for s in possible_stakes(grid, players[0], N):
+	stakes, raids = possible_stakes_and_raids(grid, players[0], N)
+	for s in stakes:
 		new_value = min_value(execute_stake(s[1], s[2], grid, players[0], N), values, players, N, depth + 1, cutoff)
 		if v < new_value:
 			v = new_value
 			move = [s[0] + " Stake", (s[1], s[2])]
 			# print move
-	for r in possible_raids(grid, players[0], N):
+	for r in raids:
 		new_value = min_value(execute_raid(r[1], r[2], grid, players[0], N), values, players, N, depth + 1, cutoff)
 		if v < new_value:
 			v = new_value
@@ -101,11 +109,14 @@ def min_value(grid, values, players, N, depth, cutoff):
 		return grid_value(grid, values, players[0], N)
 	else:
 		v = 9999
-		for s in possible_stakes(grid, players[1], N):
+		stakes, raids = possible_stakes_and_raids(grid, players[1], N)
+		if stakes == [] and raids == []:
+			return grid_value(grid, values, players[0], N)
+		for s in stakes:
 			new_value = max_value(execute_stake(s[1], s[2], grid, players[1], N), values, players, N, depth + 1, cutoff)
 			if v > new_value:
 				v = new_value
-		for r in possible_raids(grid, players[1], N):
+		for r in raids:
 			new_value = max_value(execute_raid(r[1], r[2], grid, players[1], N), values, players, N, depth + 1, cutoff)
 			if v > new_value:
 				v = new_value
@@ -117,11 +128,14 @@ def max_value(grid, values, players, N, depth, cutoff):
 		return grid_value(grid, values, players[0], N)
 	else:
 		v = -9999
-		for s in possible_stakes(grid, players[0], N):
+		stakes, raids = possible_stakes_and_raids(grid, players[0], N)
+		if stakes == [] and raids == []:
+			return grid_value(grid, values, players[0], N)
+		for s in stakes:
 			new_value = min_value(execute_stake(s[1], s[2], grid, players[0], N), values, players, N, depth + 1, cutoff)
 			if v < new_value:
 				v = new_value
-		for r in possible_raids(grid, players[0], N):
+		for r in raids:
 			new_value = min_value(execute_raid(r[1], r[2], grid, players[0], N), values, players, N, depth + 1, cutoff)
 			if v < new_value:
 				v = new_value
@@ -141,28 +155,71 @@ def grid_value(grid, values, player, N):
 
 	return player_score
 
-def possible_stakes(grid, player, N):
+# def possible_stakes(grid, player, N):
 
-	stakes = []
+# 	stakes = []
 
-	for r in xrange(0, N):
-		for c in xrange(0, N):
-			#print grid
-			if grid[r][c] == '.':
-				stakes.append((chr(ord('A') + c) + str(r + 1), r, c))
+# 	for r in xrange(0, N):
+# 		for c in xrange(0, N):
+# 			#print grid
+# 			if grid[r][c] == '.':
+# 				stakes.append((chr(ord('A') + c) + str(r + 1), r, c))
 
-	return stakes
+# 	return stakes
 
-def possible_raids(grid, player, N):
+# def possible_raids(grid, player, N):
 	
+# 	raids = []
+
+# 	for r in xrange(0, N):
+# 		for c in xrange(0, N):
+# 			if grid[r][c] == player:
+# 				raids += check_raids(r, c, grid, player, N)
+
+# 	return list(set(raids))
+
+def possible_stakes_and_raids(grid, player, N):
+	
+	stakes = []
 	raids = []
 
 	for r in xrange(0, N):
 		for c in xrange(0, N):
-			if grid[r][c] == player:
-				raids += check_raids(r, c, grid, player, N)
+			if grid[r][c] == '.':
+				if check_for_raids(r, c, grid, player, N):
+					raids.append((chr(ord('A') + c) + str(r + 1), r, c))
+				else:
+					stakes.append((chr(ord('A') + c) + str(r + 1), r, c))
+	# 		elif grid[r][c] == player:
+	# 			raids += check_raids(r, c, grid, player, N)
+	# raids = list(set(raids))
 
-	return list(set(raids))
+	return stakes, raids
+
+def check_for_raids(r, c, grid, player, N):
+
+	player_count = 0
+	opponent_count = 0
+
+	for j in [-1, 1]:
+		if c + j >= 0 and c + j < N:
+			if grid[r][c + j] == player:
+				player_count += 1
+			elif grid[r][c + j] != '.':
+				opponent_count += 1
+
+	for i in [-1, 1]:
+		if r + i >= 0 and r + i < N:
+			if grid[r + i][c] == player:
+				player_count += 1
+			elif grid[r + i][c] != '.':
+				opponent_count += 1
+
+	if player_count > 0 and opponent_count > 0:
+		return True
+	else:
+		return False
+				
 
 def check_raids(r, c, grid, player, N):
 	
@@ -170,26 +227,42 @@ def check_raids(r, c, grid, player, N):
 
 	for i in [-1, 1]:
 		if r + i >= 0 and r + i < N:
-			if grid[r + i][c] == '.':
+			if grid[r + i][c] == '.' and has_opponents_nearby(r + i, c, grid, player, N):
 				raids.append((chr(ord('A') + c) + str(r + i + 1), r + i, c))
 
 	for j in [-1, 1]:
 		if c + j >= 0 and c + j < N:
-			if grid[r][c + j] == '.':
+			if grid[r][c + j] == '.' and has_opponents_nearby(r, c + j, grid, player, N):
 				raids.append((chr(ord('A') + c + j) + str(r + 1), r, c + j))
 
 	return raids
 
+def has_opponents_nearby(r, c, grid, player, N):
+	
+	for j in [-1, 1]:
+		if c + j >= 0 and c + j < N:
+			if grid[r][c + j] != '.' and grid[r][c + j] != player:
+				return True
+
+	for i in [-1, 1]:
+		if r + i >= 0 and r + i < N:
+			if grid[r + i][c] != '.' and grid[r + i][c] != player:
+				return True
+
+	return False
+
 def execute_stake(r, c, grid, player, N):
 	
-	new_grid = map(list, grid)
+	# new_grid = map(list, grid)
+	new_grid = [row[:] for row in grid]
 	new_grid[r][c] = player
 
 	return new_grid
 
 def execute_raid(r, c, grid, player, N):
 	
-	new_grid = map(list, grid)
+	# new_grid = map(list, grid)
+	new_grid = [row[:] for row in grid]
 	new_grid[r][c] = player
 
 	for i in [-1, 1]:
@@ -224,6 +297,7 @@ cutoff = int(input_file.readline().rstrip('\n'))
 values = []
 grid = []
 
+
 for r in xrange(0,N):
 	data = input_file.readline().rstrip('\n').split(' ')
 	row = [int(x) for x in data]
@@ -234,6 +308,16 @@ for r in xrange(0,N):
 	data = input_file.readline().rstrip('\n')
 	row = [x for x in data]
 	grid.append(row)
+
+
+# t1 = time.time()
+# new_grid = [row[:] for row in grid]
+# t2 = time.time()
+# new_grid = map(list, grid)
+# t3 = time.time()
+
+# print t2 - t1
+# print t3 - t2
 
 players = [player, get_opponent(player)]
 
